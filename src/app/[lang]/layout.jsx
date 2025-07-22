686a749aa1f69c8063f98846dbf08db6ccc38bdc
Prashan5903s <chaubeyprashant498@gmail.com>
2025-07-22 13:45:00 +0530

    Changes of logo

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -25,9 +25,8 @@ import '@/app/globals.css'
 import '@assets/iconify-icons/generated-icons.css'
 
 export const metadata = {
-  title: 'Vuexy - MUI Next.js Admin Dashboard Template',
-  description:
-    'Vuexy - MUI Next.js Admin Dashboard Template - is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.'
+  title: 'DW LMS',
+  description: 'DW LMS'
 }
 
 const RootLayout = async props => {
