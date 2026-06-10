b51c47efd5d5fff3e2ad5f4602acbbd114872c9c
Prashan5903s <chaubeyprashant498@gmail.com>
2026-06-10 17:34:17 +0530

    Changes of survey

--- src/app/[lang]/(dashboard)/(private)/apps/content-data/page.jsx
+++ src/app/[lang]/(dashboard)/(private)/apps/content-data/page.jsx
@@ -140,8 +140,6 @@ const ContentData = () => {
 
       if (res.ok) {
 
-        console.log("values 1", values?.is_survey_completed, values?.completed)
-
         setSurveyModalOpen(values?.is_survey_completed && values?.completed);
       }
 
@@ -167,8 +165,6 @@ const ContentData = () => {
       if (response.ok) {
 
         const values = result?.data;
-
-        console.log("values 2", values?.is_survey_completed, values?.completed)
 
         setSurveyModalOpen(values?.is_survey_completed && values?.completed);
       }
@@ -320,8 +316,6 @@ const ContentData = () => {
 
         }
 
-        console.log("values 3", values?.is_survey_completed, values?.completed)
-
         setSurveyModalOpen(values?.is_survey_completed && values?.completed)
 
 
@@ -339,8 +333,6 @@ const ContentData = () => {
         }
 
         const values = res?.data;
-
-        console.log("values 4", values?.is_survey_completed, values?.completed)
 
         setSurveyModalOpen(values?.is_survey_completed && values?.completed)
       }
